466b7714f78de309d69fbcb57c3021cdb4fcdb81
qhren <qihong_ren@163.com>
2018-12-21 10:46:23 +0800

    删除多余文件夹

--- Classification on Mnist Data/Fully_Connected_Network/Fully_Connected_Net/Soft_Max.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def soft_max(a):
-    return np.exp(a)/np.sum(np.exp(a))
-
--- Classification on Mnist Data/Fully_Connected_Network/Fully_Connected_Net/sigmoid_prime.py
+++
@@ -1,6 +0,0 @@
-from sigmoid import sigmoid
-
-
-def sigmoid_prime(z):
-    return sigmoid(z) * (1 - sigmoid(z))
-
